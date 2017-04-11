234f50633968be05ac5e3d77e42d27ee90b221a2
Yvan Pangilinan <yvanpangilinan@Yvans-MacBook.local>
2017-04-11 11:03:20 -0400

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,37 @@
+// const MongoClient = require(`mongodb`).MongoClient;
+const {MongoClient, ObjectID} = require(`mongodb`);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('unable to connect to MongoDB server');
+  }
+  console.log('connected to MongoDB server');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+  //   console.log(result);
+  // })
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').deleteMany({name: 'Yvan'}).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('58ece45b7907b4b4f2c7c1ee')
+  }).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2);
+  })
+
+  // db.close();
+});
